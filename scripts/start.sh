72b909d6ab41177dfc1976c10d8d25c4c5dbde68
manav-makhija77 <104092605+manav-makhija77@users.noreply.github.com>
2025-03-26 17:41:25 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,4 +8,4 @@ aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS
 docker pull 205930641078.dkr.ecr.ap-southeast-1.amazonaws.com/flask-app-pipeline:latest
 
 echo "Starting new container..."
-docker run -d --name flask-app -p 5001:5000 205930641078.dkr.ecr.ap-southeast-1.amazonaws.com/flask-app-pipeline:latest
+docker run -d --name flask-app -p 5000:5000 205930641078.dkr.ecr.ap-southeast-1.amazonaws.com/flask-app-pipeline:latest
